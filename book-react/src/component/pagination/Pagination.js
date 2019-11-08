14844f9cef8e12e98adf1e5f8886e994f28a8439
thanght1 <v.thanght1@vinsmart.net.vn>
2019-11-08 17:59:49 +0700

    no message

---
+++ book-react/src/component/pagination/Pagination.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Pagination.css';
+import ic_up_arrow from '../../asset/ic_up_arrow.svg';
+import ic_up_arrows from '../../asset/ic_up_arrows.svg';
+import ic_down_arrow from '../../asset/ic_down_arrow.svg';
+import ic_down_arrows from '../../asset/ic_down_arrows.svg';
+
+class Pagination extends Component {
+  render() {
+    const max = 15;
+    const current = 5;
+    const first = current < 5 ? true : false;
+    const last = max - current > 3 ? true : false;
+    const items;
+    if (first) {
+      items = [1, 2, 3, 4, 5];
+    }
+
+    return (
+      <div className="pagination-container">
+        <span>
+          <img src={ic_up_arrow} />
+        </span>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>...</span>
+        <span>100</span>
+        <span>
+          <img src={ic_down_arrow} />
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Pagination;
--- book-react/src/page/home/Home.js
+++ book-react/src/page/home/Home.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import Pagination from '../../component/pagination/Pagination';
+
 import cover from '../../asset/cover.jpeg';
 import './Home.css';
 
-const bookList = [{ name: 'Truyen 1' }, { name: 'Truyen 2' }];
+const bookList = [
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' },
+  { name: 'Java 8 in Action' }
+];
 
 class Home extends Component {
   render() {
     return (
       <div className="home-container">
-        <div class="book-item">
-          <div className="book-image"></div>
-          <div className="book-info">
-            <p>Title</p>
-            <p>Description</p>
+        <div className="book-container">
+          <div className="book-menu">
+            <span className="book-menu-active">NEW</span>
+            <span>HOT</span>
+            <span>MOST</span>
+          </div>
+          <div className="book-list">
+            {bookList.map(book => {
+              return (
+                <div class="book-item">
+                  <img src={cover} className="book-image" />
+                  <div className="book-info">
+                    <span className="book-name">{book.name}</span>
+                    <div className="book-chapter">
+                      <span>Chương 2</span>
+                      <span>Chương 1</span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
+        <Pagination />
       </div>
     );
   }
